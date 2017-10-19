43301e0876e577e78ad2beb069c274a7d5d7b821
harttle <harttle@harttle.com>
2017-10-19 19:28:43 +0800

    init

---
+++ common.js
@@ -0,0 +1,9 @@
+require.config({
+  baseUrl: '/amd_modules'
+})
+
+require(['ralltiir', 'ralltiir-application/service'], function (rt, Service) {
+  rt.services.register('/', {}, Service)
+  rt.services.register('/profile', {title: {html: 'My Profile'}}, Service)
+  rt.action.start()
+})
